7e9866c2fd4e8adf66e58beb73a848ed830a42e1
JcKaps <kapalatajc@gmail.com>
2025-12-06 00:07:35 +0200

    suppression du composant whatsapp redirect

--- src/app/Achat/achat.ts
+++ src/app/Achat/achat.ts
@@ -1,11 +1,9 @@
 import { Routes } from "@angular/router";
 import { Panier } from "./panier/panier";
-import { WhatsappRedirect } from "./whatsapp-redirect/whatsapp-redirect";
 import { AchatDirect } from "./achat-direct/achat-direct";
 
 
 export const Achats: Routes = [
-    { path: 'achat/watsapp-commande', component: WhatsappRedirect},
     { path: 'achat/achat-direct/:id', component: AchatDirect},
     { path: 'achat/panier', component: Panier}
 ]
